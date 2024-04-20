3ceafd4b851c433c91d6b46d67a0cab5f5f8665d
GCKeita <gckeita24@gmail.com>
2024-04-20 12:55:28 -0400

    03-vars.sh

--- 03-vars.sh
+++ 03-vars.sh
@@ -4,3 +4,11 @@ echo Value of a is $a dollars
 
 # Alternatively, variables can also be accessed by using ${a}
 echo Value of a is ${a}dollars
+
+
+# Special variables for inputs
+# Specail variables used for inputs are $0 to $n, $* , $#
+echo $0
+echo $1
+echo $*
+echo $#
